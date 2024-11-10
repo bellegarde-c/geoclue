2f578dd7959c153fd085514cdbf20a7c71fb2aaf
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-11-10 20:05:07 +0100

    hybris: move to GClueLocationSource{Stop,Start}Result
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/gclue-hybris-source.c
+++ src/gclue-hybris-source.c
@@ -40,9 +40,9 @@ G_DEFINE_TYPE_WITH_CODE (GClueHybrisSource,
                          GCLUE_TYPE_LOCATION_SOURCE,
                          G_ADD_PRIVATE (GClueHybrisSource))
 
-static gboolean
+static GClueLocationSourceStartResult
 gclue_hybris_source_start (GClueLocationSource *source);
-static gboolean
+static GClueLocationSourceStopResult
 gclue_hybris_source_stop (GClueLocationSource *source);
 
 static void
@@ -201,34 +201,38 @@ gclue_hybris_source_get_singleton (void)
         return source;
 }
 
-static gboolean
+static GClueLocationSourceStartResult
 gclue_hybris_source_start (GClueLocationSource *source)
 {
         GClueLocationSourceClass *base_class;
+        GClueLocationSourceStartResult base_result;
 
         g_return_val_if_fail (GCLUE_IS_HYBRIS_SOURCE (source), FALSE);
 
         base_class = GCLUE_LOCATION_SOURCE_CLASS (gclue_hybris_source_parent_class);
-        if (!base_class->start (source))
-                return FALSE;
+        base_result = base_class->start (source);
+        if (base_result != GCLUE_LOCATION_SOURCE_START_RESULT_OK)
+                return base_result;
 
         connect_to_service (GCLUE_HYBRIS_SOURCE (source));
 
-        return TRUE;
+        return base_result;
 }
 
-static gboolean
+static GClueLocationSourceStopResult
 gclue_hybris_source_stop (GClueLocationSource *source)
 {
         GClueLocationSourceClass *base_class;
+        GClueLocationSourceStartResult base_result;
 
         g_return_val_if_fail (GCLUE_IS_HYBRIS_SOURCE (source), FALSE);
 
         base_class = GCLUE_LOCATION_SOURCE_CLASS (gclue_hybris_source_parent_class);
-        if (!base_class->stop (source))
-                return FALSE;
+        base_result = base_class->start (source);
+        if (base_result != GCLUE_LOCATION_SOURCE_STOP_RESULT_OK)
+                return base_result;
 
         disconnect_from_service (GCLUE_HYBRIS_SOURCE (source));
 
-        return TRUE;
+        return base_result;
 }
